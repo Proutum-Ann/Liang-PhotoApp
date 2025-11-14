0ef5b9ead7fe22691cdd3f5c965b4ac1a557f124
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-11-14 15:59:08 +0000

    added Pebble to showcase slide, updated the dropdown details

--- gallery.js
+++ gallery.js
@@ -1,6 +1,6 @@
 let mCurrentIndex = 0 // Tracks the current image index
 let mImages = [] // Array to hold GalleryImage objects
-const mUrl = 'images.json' // Replace with actual JSON URL
+const mUrl = 'characters.json' // Replace with actual JSON URL
 const mWaitTime = 5000 // Timer interval in milliseconds
 
 $(document).ready(() => {
@@ -31,9 +31,9 @@ function fetchJSON () {
     async: true,
     datatype: 'json',
     success: data => {
-      mImages = data.images
+      mImages = data.characters
       swapPhoto()
-      startTimer()
+      //startTimer()
     }
   })
   // On success, parse the JSON and push each image object into mImages array
@@ -46,10 +46,21 @@ function swapPhoto () {
   mImages[mCurrentIndex]
   // Update the #photo element's src attribute with the current image's path
   $('#photo').attr('src', mImages[mCurrentIndex].imgPath)
+  $('#photo').css("maxWidth", "300px")
   // Update the .location, .description, and .date elements with the current image's details
-  $('.location').text('Location: ' + mImages[mCurrentIndex].imgLocation)
+  $('.banner').text(mImages[mCurrentIndex].charName)
+  $('.descText').text(mImages[mCurrentIndex].charDesc)
+  $('.healthStat').text(mImages[mCurrentIndex].stats.health)
+  $('.checkStat').text(mImages[mCurrentIndex].stats.skillCheck)
+  $('.speedStat').text(mImages[mCurrentIndex].stats.speed)
+  $('.stamStat').text(mImages[mCurrentIndex].stats.stamina)
+  $('.stealthStat').text(mImages[mCurrentIndex].stats.stealth)
+  $('.extraStat').text(mImages[mCurrentIndex].stats.extraction)
+
+  //old code
+  /* $('.location').text('Location: ' + mImages[mCurrentIndex].imgLocation)
   $('.description').text('Description: ' + mImages[mCurrentIndex].description)
-  $('.date').text('Date: ' + mImages[mCurrentIndex].date)
+  $('.date').text('Date: ' + mImages[mCurrentIndex].date) */
 }
 
 // Advances to the next photo, loops to the first photo if the end of array is reached
